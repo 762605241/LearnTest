3a66e02fded2f383be3863f96402c32e8dda4527
liuxiaoliang <1234567890>
2020-07-26 23:38:50 +0800

    实体类不定义无参构造方法，默认会自动生成一个无参构造方法。但写了有参构造方法，就不会自动生成无参构造方法。

---
+++ Test/src/main/java/com/lxl/pojo/Pojo.java
@@ -0,0 +1,11 @@
+package com.lxl.pojo;
+
+public class Pojo {
+    private String name;
+    private Integer age;
+
+    public Pojo(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ Test/src/main/java/com/lxl/pojo/Test.java
@@ -0,0 +1,5 @@
+package com.lxl.pojo;
+
+public class Test {
+    Pojo pojo = new Pojo("a", 1);
+}
